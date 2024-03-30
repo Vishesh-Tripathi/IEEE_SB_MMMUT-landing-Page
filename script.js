7a01e12f167dc8ff4fe4ca41a52a6ce877c15aae
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-03-30 16:37:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var typed = new Typed(".multiple_text", {
+    strings:[
+        "IEEE SB | MMMUT",
+        
+        ],
+    // strings: typed_strings.split(", "),
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
+var loader = document.getElementById("preloder");
+window.addEventListener("load",function(){
+    loader.style.display="none";
+})
+
+var counter =1;
+       setInterval(function(){
+        document.getElementById('radio'+counter).checked=true;
+        counter++;
+        if(counter>7){
+            counter=1;
+        }
+       }, 5000);
+
+
+
+    
